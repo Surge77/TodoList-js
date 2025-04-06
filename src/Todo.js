7e56e7db84bfee502b92b121e2422502c6f45344
TEJAS <tejasdeshmane66@gmail.com>
2025-04-06 19:02:24 +0530

    Updated

--- src/Todo.js
+++ src/Todo.js
@@ -7,20 +7,33 @@ function renderTodoList(){
   //Here we used accumulator pattern
   let todoListHTML = '';
 
-  for(let i = 0; i < todoList.length; i++){
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
+  //ForEach loop is easier to read than for loop for arrays
+  todoList.forEach((todoObject,index) => {
     const {name, dueDate} = todoObject; //Destructuring
     todoListHTML += `
       <div>${name}</div>
       <div>${dueDate}</div>
       <button class='delete-todo-button' onclick='
-        todoList.splice(${i},1); //1 is the number of elements we want to delete
+        todoList.splice(${index},1); //1 is the number of elements we want to delete
         renderTodoList();
       '>Delete</button>
        `; //We generated the HTML for each element in the array
-  }
+  })
+
+  // for(let i = 0; i < todoList.length; i++){
+  //   const todoObject = todoList[i];
+  //   // const name = todoObject.name;
+  //   // const dueDate = todoObject.dueDate;
+  //   const {name, dueDate} = todoObject; //Destructuring
+  //   todoListHTML += `
+  //     <div>${name}</div>
+  //     <div>${dueDate}</div>
+  //     <button class='delete-todo-button' onclick='
+  //       todoList.splice(${i},1); //1 is the number of elements we want to delete
+  //       renderTodoList();
+  //     '>Delete</button>
+  //      `; //We generated the HTML for each element in the array
+  // }
 
   document.querySelector('.js-todoList').innerHTML = todoListHTML; //We render the HTML to the DOM 
 
